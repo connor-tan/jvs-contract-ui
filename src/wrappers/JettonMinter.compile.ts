a205e5ea193518e94de151432c5921dff3a20110
谈佳梁 <jialiang.tan@fanttec.com>
2024-07-01 23:17:00 +0800

    init

--- src/wrappers/JettonMinter.compile.ts
+++
@@ -1,5 +0,0 @@
-import { type CompilerConfig } from '@ton/blueprint';
-
-export const compile: CompilerConfig = {
-    targets: ['contracts/JettonMinterDiscoverable.fc','ft/params.fc','ft/op-codes.fc','ft/discovery-params.fc','ft/jetton-utils.fc'],
-};
--- src/wrappers/Locker.compile.ts
+++
@@ -1,6 +0,0 @@
-import { type CompilerConfig } from '@ton/blueprint';
-
-export const compile: CompilerConfig = {
-    lang: 'func',
-    targets: ['contracts/Locker.fc'],
-};
